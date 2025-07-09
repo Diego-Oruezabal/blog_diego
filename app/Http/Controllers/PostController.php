28ca420ebd8c0dc1963a2857573611fdcbc320a2
Diego Oruezabal <diegooruezabal@gmail.com>
2025-07-09 17:47:08 +0200

    Migracion de template a laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+{
+    $posts = Post::with(['user', 'categories'])
+                 ->latest('published_at')
+                 ->take(4)
+                 ->get();
+
+    return view('welcome', compact('posts'));
+}
 }
