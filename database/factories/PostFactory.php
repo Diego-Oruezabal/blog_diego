a23db8d657dd5f870aa8a1db0d33967dfd5d7fd9
Diego Oruezabal <diegooruezabal@gmail.com>
2025-07-10 21:14:48 +0200

    imagenes en todos los posts

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -16,6 +16,28 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $exampleImages = [
+            'blog/1.jpg',
+            'blog/2.jpg',
+            'blog/3.jpg',
+            'blog/4.jpg',
+            'blog/5.jpg',
+            'blog/6.jpg',
+            'blog/7.jpg',
+            'blog/8.jpg',
+            'blog/9.jpg',
+            'blog/10.jpg',
+            'blog/11.jpg',
+            'blog/12.jpg',
+            'blog/13.jpg',
+            'blog/14.jpg',
+            'blog/15.jpg',
+            'blog/16.jpg',
+            'blog/17.jpg',
+            'blog/18.jpg',
+            'blog/19.jpg',
+            'blog/20.jpg',
+        ];
 
 
     return [
@@ -25,7 +47,7 @@ class PostFactory extends Factory
         'summary' => $this->faker->sentence(),
         'content' => $this->faker->paragraphs(3, true),
         'status' => $this->faker->randomElement(['draft', 'published', 'archived']),
-        'featured_image' => $this->faker->imageUrl(640, 480, 'nature', true, 'Faker'),
+        'featured_image' => $this->faker->randomElement($exampleImages),
         'created_at' => now(),
         'updated_at' => now(),
         'published_at' => now(), // Assuming you want to set this to now for published
