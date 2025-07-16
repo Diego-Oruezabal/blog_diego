ca9b7eedcaba42fc749509254bd9a598a1c018cd
Diego Oruezabal <diegooruezabal@gmail.com>
2025-07-16 17:24:28 +0200

    CRUD Listar mis posts iconos

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -7,9 +7,10 @@
         <section class="banner" style="margin-bottom: 300px;">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-8">
+                    <div class="col-lg-12">
                         <div class="banner__content ">
                            <h2 class="mb-4">Mis publicaciones</h2>
+                           <a href="#" class="mb-3 btn btn-primary">Crear nuevo post</a>
 
                                     @if($posts->count())
                                         <table class="table table-striped">
@@ -34,12 +35,12 @@
                                                         </td>
                                                         <td>{{ $post->published_at ? $post->published_at->format('d/m/Y') : 'Sin publicar' }}</td>
                                                         <td>
-                                                            <a href="{{ route('posts.show', [$post->id, $post->slug]) }}" class="btn btn-sm btn-info" target="_blank">Ver</a>
-                                                            <a href="#" class="btn btn-sm btn-warning">Editar</a>
+                                                            <a href="{{ route('posts.show', [$post->id, $post->slug]) }}" class="btn btn-sm btn-info" target="_blank"><i class="bi bi-eye"></i></a>
+                                                            <a href="#" class="btn btn-sm btn-warning"><i class="bi bi-pen"></i></a>
                                                             <form action="#" method="POST" style="display:inline;">
                                                                 @csrf
                                                                 @method('DELETE')
-                                                                <button class="btn btn-sm btn-danger" onclick="return confirm('¿Eliminar este post?')">Eliminar</button>
+                                                                <button class="btn btn-sm btn-danger" onclick="return confirm('¿Eliminar este post?')"><i class="bi bi-trash3"></i></button>
                                                             </form>
                                                         </td>
                                                     </tr>
@@ -52,7 +53,7 @@
                                         <p class="text-muted">No has creado publicaciones todavía.</p>
                                     @endif
 
-                            <a href="#" class="mt-3 btn btn-primary">Crear nuevo post</a>
+
                         </div>
                     </div>
                 </div>
